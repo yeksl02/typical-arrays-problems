31e5779530d3acdb90913b577b781f2e382a73a1
yeksl02 <you@example.com>
2020-10-02 17:42:26 +0300

    feat add max min avg func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let min = 0;
+  if (Array.isArray(array) && array.length > 0) {
+    array.forEach((i) => {
+      min >= i ? min = i : min;
+    })
+  } else {
+    return false;
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+  if (Array.isArray(array) && array.length > 0) {
+    let max = array[0];
+    array.forEach((i)=>{
+      i > max ? max = i : max;
+    })
+    return max;
+
+  } else {
+    return 0;
+  }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let avg = 0;
+  if (Array.isArray(array) && array.length > 0) {
+
+    array.forEach((i) => {
+      avg = avg + i;
+    })
+
+  } else {
+    return false;
+
+  }
+  if (avg / array.length != NaN) {
+    return avg / array.length;
+  } else return false;
+
 }
